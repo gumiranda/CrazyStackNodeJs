7f7345666dace28e8c1ba9ff9299e52f173d5e33
Gustavo <gustavo.aguiar@redspark.io>
2022-05-03 14:29:08 -0300

    Aula 7 - Gerador de teste genérico

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -6,8 +6,41 @@ module.exports = function (plop) {
         ],
         actions: [...entitiesCreations],
     });
+    plop.setGenerator("useCases", {
+        description: "Create a new useCase",
+        prompts: [
+            { type: "input", name: "name", message: "What is the name of the useCase?" },
+        ],
+        actions: [...useCasesCreations],
+    });
+    plop.setGenerator("test", {
+        description: "Create a new test",
+        prompts: [
+            { type: "input", name: "name", message: "What is the name of the file?" },
+            { type: "input", name: "entity", message: "What is the name of the entity?" },
+            { type: "input", name: "layer", message: "What is the name of the layer?" },
+        ],
+        actions: [
+            {
+                type: "add",
+                path: "../src/slices/{{camelCase entity}}/{{camelCase layer}}/{{pascalCase name}}.spec.ts",
+                templateFile: "./templates/test.spec.ts.hbs",
+            },
+        ],
+    });
 };
-
+const useCasesCreations = [
+    {
+        type: "add",
+        path: "../src/slices/{{camelCase name}}/repositories/contracts/index.ts",
+        templateFile: "./templates/repositories/contracts/index.ts.hbs",
+    },
+    {
+        type: "add",
+        path: "../src/slices/{{camelCase name}}/repositories/contracts/add{{pascalCase name}}Repository.ts",
+        templateFile: "./templates/repositories/contracts/addDomainRepository.ts.hbs",
+    },
+];
 const entitiesCreations = [
     {
         type: "add",
