124bca7bf5075941c24e104950d168dcfa468908
Gustavo <gustavo.aguiar@redspark.io>
2022-05-04 17:20:24 -0300

    Hotfix - Corrigindo Jest Config File

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,23 @@
 module.exports = {
-  bail: 1,
-  clearMocks: true,
-  collectCoverage: true,
-  setupFiles: ["dotenv/config"],
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["src/**/*.{ts}"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
-  moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
-  },
+    bail: 1,
+    clearMocks: true,
+    collectCoverage: true,
+    setupFiles: ["dotenv/config"],
+    roots: ["<rootDir>/src"],
+    collectCoverageFrom: [
+        "<rootDir>/src/**/*.ts",
+        "!**/test/**",
+        "!**/index.ts",
+        "!**/*test.ts",
+        "!**/*spec.ts",
+        "!**/*specdb.ts",
+    ],
+    coverageDirectory: "coverage",
+    testEnvironment: "node",
+    transform: {
+        ".+\\.ts$": "ts-jest",
+    },
+    moduleNameMapper: {
+        "@/(.*)": "<rootDir>/src/$1",
+    },
 };
