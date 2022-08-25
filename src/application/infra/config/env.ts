db537e297dca160878f07e59d34894091eeffb6b
Gustavo <gustavo.aguiar@redspark.io>
2022-08-25 09:53:46 -0300

    Aula 56 - Hello World no Fastify

--- src/application/infra/config/env.ts
+++ src/application/infra/config/env.ts
@@ -1,4 +1,5 @@
 export const env = {
   mongoUri: process.env.MONGO_URL ?? "mongodb://127.0.0.1:56328",
   jwtSecret: process.env.JWT_SECRET ?? "secret",
+  port: process.env.PORT ?? 8080,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 import "./application/infra/config/module-alias";
 import { env } from "@/application/infra";
+import Fastify, { FastifyInstance } from "fastify";
+
+const fastify: FastifyInstance = Fastify({ logger: true });
+// Declare a route
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    const port: any = env?.port ?? 3000;
+    await fastify.listen({ port });
+    fastify.log.info(`server listening on ${port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
