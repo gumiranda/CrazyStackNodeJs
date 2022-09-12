e584aea62256a24705ce710c26dfb74c312581aa
Gustavo <gustavo.aguiar@redspark.io>
2022-09-12 10:21:37 -0300

    Aula 73 - Atualizando UpdateCategory UseCase

--- src/slices/category/useCases/updateCategory/UpdateCategory.ts
+++ src/slices/category/useCases/updateCategory/UpdateCategory.ts
@@ -1,16 +1,24 @@
 import { UpdateCategoryRepository } from "@/slices/category/repositories";
 import { CategoryData } from "@/slices/category/entities";
 import { Query } from "@/application/types";
+import { cleanDataObject } from "@/application/helpers";
 
 export type UpdateCategory = (
-    query: Query,
-    data: CategoryData
+  query: Query,
+  data: CategoryData
 ) => Promise<CategoryData | null>;
 export type UpdateCategorySignature = (
-    updateCategory: UpdateCategoryRepository
+  updateCategory: UpdateCategoryRepository
 ) => UpdateCategory;
 export const updateCategory: UpdateCategorySignature =
-    (updateCategoryRepository: UpdateCategoryRepository) =>
-    async (query: Query, data: CategoryData) => {
-        return updateCategoryRepository.updateCategory(query, data);
-    };
+  (updateCategoryRepository: UpdateCategoryRepository) =>
+  async (query: Query, data: CategoryData) => {
+    return updateCategoryRepository.updateCategory(
+      query,
+      cleanDataObject({
+        forbiddenFields: ["_id", "createdById", "active"],
+        allowedFields: ["name", "description", "image", "updatedAt"],
+        bodyObject: data,
+      })
+    );
+  };
