631a8f7f7b165243f7773e1ec9b76bb05f620ead
gustavo.aguiar <gustavo.aguiar@redspark.io>
2022-12-18 16:46:35 -0300

    update project

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,11 @@
 module.exports = {
     mongodbMemoryServerOptions: {
-        instance: {
-            dbName: "jest",
-        },
-        binary: {
-            version: "4.0.3", // Version 4.0.3 is the latest stable release
-            skipMD5: true,
-        },
+      binary: {
+        version: "4.0.3", // Version 4.0.3 is the latest stable release
+        skipMD5: true,
+      },
+        instance: {},
         autoStart: false,
     },
+    useSharedDBForAllJestWorkers:false
 };
