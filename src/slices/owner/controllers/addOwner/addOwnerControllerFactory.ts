9b0c5d86d5f457bcb4e28cac098896cf8109b45b
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-01-05 10:03:48 -0300

    ajustando factories

--- src/slices/owner/controllers/addOwner/addOwnerControllerFactory.ts
+++ src/slices/owner/controllers/addOwner/addOwnerControllerFactory.ts
@@ -3,6 +3,7 @@ import { makeValidationComposite } from "@/application/factories";
 import { Controller } from "@/application/infra/contracts";
 import { makeAddOwnerFactory, makeLoadOwnerFactory } from "@/slices/owner/useCases";
 import { AddOwnerController } from "@/slices/owner/controllers";
+import { makeUpdateUserFactory } from "@/slices/user/useCases";
 
 export const makeAddOwnerController = (): Controller => {
   const requiredFields = [
@@ -18,7 +19,8 @@ export const makeAddOwnerController = (): Controller => {
     new AddOwnerController(
       makeValidationComposite(requiredFields),
       makeAddOwnerFactory(),
-      makeLoadOwnerFactory()
+      makeLoadOwnerFactory(),
+      makeUpdateUserFactory()
     )
   );
 };
