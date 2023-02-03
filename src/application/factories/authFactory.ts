57178dc5c676e8412940275fcfe891c854011235
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-02-03 10:57:08 -0300

    reduzindo tempo de access token

--- src/application/factories/authFactory.ts
+++ src/application/factories/authFactory.ts
@@ -4,7 +4,7 @@ import { UserRepository } from "@/slices/user/repositories";
 export const makeDbAuthentication = (): Authentication => {
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
-  const jwtAdapter = new JwtAdapter(env.jwtSecret, "120d");
+  const jwtAdapter = new JwtAdapter(env.jwtSecret, "60s");
   const jwtRefreshTokenAdapter = new JwtAdapter(env.jwtRefreshSecret, "10d");
   const userMongoRepository = new MongoRepository("user");
   const userRepository = new UserRepository(userMongoRepository);
