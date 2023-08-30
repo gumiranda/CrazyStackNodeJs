e318faa7a8bd63bc50eb97ad672c34e9761d487d
gustavo.aguiar <gustavo.aguiar@redspark.io>
2023-08-30 15:46:21 -0300

    Aula 85 - Redis como cache no get de usuários

--- src/application/infra/routes/user/userRouter.ts
+++ src/application/infra/routes/user/userRouter.ts
@@ -15,9 +15,14 @@ import {
   loadUserByPageGetSchema,
   loadUserByGeoNearSchema,
 } from "./userSchema";
+import { onSendRedis, preHandlerRedis } from "../../redis";
 
 async function user(fastify: any, options: any) {
   fastify.addHook("preHandler", authLogged());
+  if (process.env.NODE_ENV === "production") {
+    fastify.addHook("preHandler", preHandlerRedis("user"));
+    fastify.addHook("onSend", onSendRedis("user"));
+  }
   fastify.post("/user/add", addUserPostSchema, addUserAdapter());
   fastify.get("/user/load", loadUserGetSchema, loadUserAdapter());
   fastify.get("/user/loadByPage", loadUserByPageGetSchema, loadUserByPageAdapter());
