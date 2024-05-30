0da0d015ff11787e3abc3d859c2a0031ce6122fb
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-05-30 16:20:29 -0300

    feat: Update appointment aggregate repository based on database configuration

--- src/slices/appointment/useCases/loadAvailableTimes/LoadAvailableTimesFactory.ts
+++ src/slices/appointment/useCases/loadAvailableTimes/LoadAvailableTimesFactory.ts
@@ -1,16 +1,27 @@
-import { makeDatabaseInstance, MongoRepository } from "@/application/infra";
+import {
+  makeDatabaseInstance,
+  MongoRepository,
+  PostgresRepository,
+} from "@/application/infra";
 import { loadAvailableTimes, LoadAvailableTimes } from "@/slices/appointment/useCases";
 import { OwnerRepository } from "@/slices/owner/repositories";
 import { ServiceRepository } from "@/slices/service/repositories";
 import { UserRepository } from "@/slices/user/repositories";
 import { AppointmentAggregateRepository } from "../../repositories/aggregates/mongodb/appointmentAggregateRepository";
 import { whiteLabel } from "@/application/infra/config/whiteLabel";
+import { AppointmentAggregatePgRepository } from "../../repositories/aggregates/postgres/appointmentAggregatePgRepository";
 
 export const makeLoadAvailableTimesFactory = (): LoadAvailableTimes => {
   return loadAvailableTimes(
-    new AppointmentAggregateRepository(new MongoRepository("appointment")),
+    makeAggregateRepository(),
     new ServiceRepository(makeDatabaseInstance(whiteLabel.database, "service")),
     new UserRepository(makeDatabaseInstance(whiteLabel.database, "users")),
     new OwnerRepository(makeDatabaseInstance(whiteLabel.database, "owner"))
   );
+};
+const makeAggregateRepository = () => {
+  if (whiteLabel.database === "mongodb") {
+    return new AppointmentAggregateRepository(new MongoRepository("appointment"));
+  }
+  return new AppointmentAggregatePgRepository(new PostgresRepository("appointment"));
 };
