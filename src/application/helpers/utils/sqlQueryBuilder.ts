58526d8aeb852eef333b9b92c05e1f032e26de59
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-05-30 16:16:30 -0300

    feat: Add SQLQueryBuilder utility class

---
+++ src/application/helpers/utils/sqlQueryBuilder.ts
@@ -0,0 +1,43 @@
+export class SQLQueryBuilder {
+  steps: any = [];
+  values: any = [];
+  tableName: string;
+  constructor(tableName: string) {
+    this.tableName = tableName;
+  }
+  match(condition: any) {
+    this.steps.push(`WHERE ${condition}`);
+    return this;
+  }
+  sort(sortObj: any) {
+    const orderBy = Object.keys(sortObj)
+      .map((key) => `"${key}" ${sortObj[key] === -1 ? "DESC" : "ASC"}`)
+      .join(", ");
+    this.steps.push(`ORDER BY ${orderBy}`);
+    return this;
+  }
+  join({ table, on }: any) {
+    this.steps.push(`JOIN "${table}" ON ${on}`);
+  }
+  project(fields: any) {
+    this.steps.push(`SELECT ${fields} FROM "${this.tableName}"`);
+    return this;
+  }
+  group({ _id, total }: any) {
+    this.steps.push(`GROUP BY ${_id}`);
+    if (total) {
+      this.steps.push(`HAVING COUNT(*) > ${total}`);
+    }
+    return this;
+  }
+
+  addValue(value: any) {
+    this.values.push(value);
+    return this;
+  }
+
+  build() {
+    const query = this.steps.join(" ");
+    return { text: query, values: this.values };
+  }
+}
