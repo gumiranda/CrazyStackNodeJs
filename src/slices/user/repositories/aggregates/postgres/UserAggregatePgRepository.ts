804f86cc1ef40165b20c156556650d404e84ed6a
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-06-13 18:24:59 -0300

    feat: Update UserAggregatePgRepository to load users by page and geolocation
    This commit updates the UserAggregatePgRepository class to handle loading users by page and geolocation in the PostgreSQL database. It implements the LoadUserByPageGeoNearRepository interface and provides the loadUserByPageGeoNear method. The method retrieves users based on the provided query parameters, including the user's logged ID. If the query does not contain the user's logged ID, it returns null. Otherwise, it returns an empty array of users and a total count of 0.

--- src/slices/user/repositories/aggregates/postgres/UserAggregatePgRepository.ts
+++ src/slices/user/repositories/aggregates/postgres/UserAggregatePgRepository.ts
@@ -1,14 +1,33 @@
 import { Repository } from "@/application/infra/contracts/repository";
 import { Query } from "@/application/types";
-
 import { LoadUserByPageGeoNearRepository } from "../../contracts/LoadUserByPageGeoNearRepository";
 import { UserPaginated } from "../../../entities";
+import { SQLQueryBuilder } from "@/application/helpers";
 export class UserAggregatePgRepository implements LoadUserByPageGeoNearRepository {
   constructor(private readonly repository: Repository) {}
   async loadUserByPageGeoNear(query: Query): Promise<UserPaginated | null> {
     if (!query?.options?.userLoggedId) {
       return null;
     }
-    return { users: [], total: 0 };
+    const page = query?.options?.page ?? 0;
+    const limitPerPage = query?.options?.limitPerPage ?? 10;
+    const queryBuilded = new SQLQueryBuilder("users")
+      .projectWithDistance(query?.fields?.lng, query?.fields?.lat)
+      .match(
+        "users.coord->'coordinates'->>0 IS NOT NULL AND users.coord->'coordinates'->>1 IS NOT NULL AND users._id != $3 "
+      )
+      .addValue(query?.options?.userLoggedId)
+      .sortByDistance(query?.fields?.lng, query?.fields?.lat)
+      .skip((page - 1) * limitPerPage)
+      .limit(limitPerPage)
+      .build();
+    const result = await this.repository.aggregate(queryBuilded);
+    return { users: mapPassword(result ?? []), total: limitPerPage };
   }
 }
+export const mapPassword = (users: any) => {
+  return users.map((user: any) => {
+    delete user.password;
+    return user;
+  });
+};
