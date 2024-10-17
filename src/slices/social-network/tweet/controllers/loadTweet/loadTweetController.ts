9e6f9ed18ec78eb14ccb9081fc98efbb391d3908
gustavo.aguiar <imartytk@hotmail.com>
2024-10-17 15:27:03 -0300

    Refatorar consulta getOne para adicionar opções de projeção e inclusão

--- src/slices/social-network/tweet/controllers/loadTweet/loadTweetController.ts
+++ src/slices/social-network/tweet/controllers/loadTweet/loadTweetController.ts
@@ -7,6 +7,7 @@ import {
   badRequest,
   ok,
 } from "@/application/helpers";
+import { formatFieldsByPrefix } from "@/application/helpers/utils/formatFieldsByPrefix";
 import { Controller } from "@/application/infra/contracts";
 import { LoadTweet } from "@/slices/social-network/tweet/useCases";
 
@@ -47,7 +48,7 @@ export class LoadTweetController extends Controller {
                   tweetlikeId: tweettweetliketweetlikeId,
                 })
               )
-            : null,
+            : tweetLoaded?.[0]?.tweetlike,
       };
       return ok(final);
     }
@@ -55,35 +56,3 @@ export class LoadTweetController extends Controller {
   }
 }
 const prefixes = ["users", "tweettweetlike", "tweet"];
-
-function formatFieldsByPrefix(obj: any, prefixes: string[]) {
-  const result: { [key: string]: any } = {};
-
-  // Inicializa subobjetos para cada prefixo
-  prefixes.forEach((prefix) => {
-    result[prefix] = {};
-  });
-
-  for (const key in obj) {
-    let matchedPrefix = null;
-
-    // Encontra o prefixo correspondente para a chave
-    prefixes.forEach((prefix) => {
-      if (key.startsWith(prefix)) {
-        matchedPrefix = prefix;
-      }
-    });
-
-    if (matchedPrefix) {
-      // Remove o prefixo e transforma a primeira letra do campo restante em minúscula
-      const newKey = key
-        .replace(new RegExp(`^${matchedPrefix}`), "")
-        .replace(/^./, (str) => str.toLowerCase());
-      result[matchedPrefix][newKey] = obj[key];
-    } else {
-      result[key] = obj[key]; // Mantém as chaves que não têm o prefixo
-    }
-  }
-
-  return result;
-}
