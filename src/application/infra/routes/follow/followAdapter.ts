38409a0745f4fa2c5401dca33475cb4a3cbcb69e
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-02-10 18:19:27 -0300

    fix: Update import path for follow controllers in followAdapter

--- src/application/infra/routes/follow/followAdapter.ts
+++ src/application/infra/routes/follow/followAdapter.ts
@@ -5,11 +5,10 @@ import {
   makeDeleteFollowController,
   makeUpdateFollowController,
   makeLoadFollowByPageController,
-} from "@/slices/follow/controllers";
+} from "@/slices/social-network/follow/controllers";
 
 export const addFollowAdapter = () => adaptRoute(makeAddFollowController());
 export const loadFollowAdapter = () => adaptRoute(makeLoadFollowController());
-export const loadFollowByPageAdapter = () =>
-  adaptRoute(makeLoadFollowByPageController());
+export const loadFollowByPageAdapter = () => adaptRoute(makeLoadFollowByPageController());
 export const deleteFollowAdapter = () => adaptRoute(makeDeleteFollowController());
 export const updateFollowAdapter = () => adaptRoute(makeUpdateFollowController());
