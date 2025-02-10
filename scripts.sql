e1fe07402371cb410bfa70dc2ef9cacce05b5f86
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-02-10 17:31:58 -0300

    refactor: Update foreign key constraints and modify follow table structure

--- scripts.sql
+++ scripts.sql
@@ -652,3 +652,36 @@ ALTER TABLE "tweetlike" ADD CONSTRAINT "tweetlike_tweetId_fkey" FOREIGN KEY ("tw
 
 -- AddForeignKey
 ALTER TABLE "follow" ADD CONSTRAINT "follow_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "users"("_id") ON DELETE SET NULL ON UPDATE CASCADE;
+-- DropForeignKey
+ALTER TABLE "follow" DROP CONSTRAINT "follow_createdById_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tweet" DROP CONSTRAINT "tweet_createdById_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tweet" DROP CONSTRAINT "tweet_tweetId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tweetlike" DROP CONSTRAINT "tweetlike_createdById_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tweetlike" DROP CONSTRAINT "tweetlike_tweetId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "tweet" ADD CONSTRAINT "tweet_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "users"("_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "tweet" ADD CONSTRAINT "tweet_tweetId_fkey" FOREIGN KEY ("tweetId") REFERENCES "tweet"("_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "tweetlike" ADD CONSTRAINT "tweetlike_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "users"("_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "tweetlike" ADD CONSTRAINT "tweetlike_tweetId_fkey" FOREIGN KEY ("tweetId") REFERENCES "tweet"("_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "follow" ADD CONSTRAINT "follow_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "users"("_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "follow" DROP COLUMN "user1Slug",
+DROP COLUMN "user2Slug",
+ADD COLUMN     "userId" UUID NOT NULL,
+ALTER COLUMN "createdById" SET NOT NULL;
