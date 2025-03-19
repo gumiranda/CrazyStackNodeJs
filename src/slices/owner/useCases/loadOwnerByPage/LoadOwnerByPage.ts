f0ad5ba8662db6748e030b8b2a487593fb1eba84
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-19 15:52:09 -0300

    feat: Enhance LoadOwnerByPage use case to enrich owner data with associated places

--- src/slices/owner/useCases/loadOwnerByPage/LoadOwnerByPage.ts
+++ src/slices/owner/useCases/loadOwnerByPage/LoadOwnerByPage.ts
@@ -1,12 +1,24 @@
-import { LoadOwnerByPageRepository } from "@/slices/owner/repositories";
-import { OwnerPaginated } from "@/slices/owner/entities";
 import { Query } from "@/application/types";
+import type { LoadPlace } from "@/slices/place/useCases";
+import type { LoadOwnerByPageRepository } from "../../repositories";
 
-export type LoadOwnerByPage = (query: Query) => Promise<OwnerPaginated | null>;
+export type LoadOwnerByPage = (query: Query) => Promise<any | null>;
 export type LoadOwnerByPageSignature = (
-  loadOwnerByPage: LoadOwnerByPageRepository
+  loadOwnerByPage: LoadOwnerByPageRepository,
+  loadPlace: LoadPlace
 ) => LoadOwnerByPage;
+
 export const loadOwnerByPage: LoadOwnerByPageSignature =
-  (loadOwnerByPageRepository: LoadOwnerByPageRepository) => async (query: Query) => {
-    return loadOwnerByPageRepository.loadOwnerByPage(query);
+  (loadOwnerByPageRepository, loadPlace) => async (query: Query) => {
+    const ownersPaginated = await loadOwnerByPageRepository.loadOwnerByPage(query);
+    if (!ownersPaginated?.owners) {
+      return ownersPaginated;
+    }
+    const enrichedData = await Promise.all(
+      ownersPaginated.owners.map(async (owner) => {
+        const place = await loadPlace({ fields: { ownerId: owner?._id }, options: {} });
+        return { ...owner, place };
+      })
+    );
+    return { ...ownersPaginated, owners: enrichedData };
   };
--- src/slices/owner/useCases/loadOwnerByPage/LoadOwnerByPageFactory.ts
+++ src/slices/owner/useCases/loadOwnerByPage/LoadOwnerByPageFactory.ts
@@ -2,10 +2,11 @@ import { makeDatabaseInstance } from "@/application/infra";
 import { OwnerRepository } from "@/slices/owner/repositories";
 import { loadOwnerByPage, LoadOwnerByPage } from "@/slices/owner/useCases";
 import { whiteLabel } from "@/application/infra/config/whiteLabel";
+import { makeLoadPlaceFactory } from "@/slices/place/useCases";
 
 export const makeLoadOwnerByPageFactory = (): LoadOwnerByPage => {
   const repository = new OwnerRepository(
     makeDatabaseInstance(whiteLabel.database, "owner")
   );
-  return loadOwnerByPage(repository);
+  return loadOwnerByPage(repository, makeLoadPlaceFactory());
 };
