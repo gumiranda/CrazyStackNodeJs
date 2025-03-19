f6c622643816f26f2368ed7501630fdb4592d625
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-19 08:53:35 -0300

    feat: Add UUID validation function to email validation helper

--- src/application/helpers/validation/email-validation.ts
+++ src/application/helpers/validation/email-validation.ts
@@ -13,3 +13,8 @@ export class EmailValidation implements Validation {
     return [];
   }
 }
+export const isValidUUID = (uuid: string) => {
+  const uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return uuidRegex.test(uuid);
+};
