14337e4bae5ecaf92ce101a9168f9c1eead1c53f
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-19 15:27:25 -0300

    feat: Extend PlaceEntity with additional fields and update tests

--- src/slices/place/entities/PlaceEntity.spec.ts
+++ src/slices/place/entities/PlaceEntity.spec.ts
@@ -2,46 +2,47 @@ import { PlaceEntity } from "./PlaceEntity";
 import MockDate from "mockdate";
 
 export const fakePlaceEntity = {
-    _id: "123",
-    createdById: "123",
-    name: "fakePlaceEntity",
-    active: true,
-    createdAt: new Date(),
-    updatedAt: new Date(),
+  _id: "123",
+  createdById: "123",
+  name: "fakePlaceEntity",
+  active: true,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  categoryPlaceId: "",
 };
 export const fakePlacePaginated = {
-    total: 11,
-    places: [
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-        fakePlaceEntity,
-    ],
+  total: 11,
+  places: [
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+    fakePlaceEntity,
+  ],
 };
 
 describe("Place", () => {
-    beforeAll(async () => {
-        MockDate.set(new Date());
+  beforeAll(async () => {
+    MockDate.set(new Date());
+  });
+  afterAll(async () => {
+    MockDate.reset();
+  });
+  it("can be created", () => {
+    const obj = new PlaceEntity(fakePlaceEntity);
+    expect(obj).toBeTruthy();
+    expect(obj).toEqual({
+      ...fakePlaceEntity,
+      _id: undefined,
+      active: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     });
-    afterAll(async () => {
-        MockDate.reset();
-    });
-    it("can be created", () => {
-        const obj = new PlaceEntity(fakePlaceEntity);
-        expect(obj).toBeTruthy();
-        expect(obj).toEqual({
-            ...fakePlaceEntity,
-            _id: undefined,
-            active: false,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        });
-    });
+  });
 });
--- src/slices/place/entities/PlaceEntity.ts
+++ src/slices/place/entities/PlaceEntity.ts
@@ -1,28 +1,52 @@
 export type PlaceData = {
-    _id?: string;
-    createdById: string;
-    name: string;
-    active?: boolean;
-    createdAt?: Date;
-    updatedAt?: Date;
+  _id?: string;
+  createdById: string;
+  name: string;
+  active?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  categoryPlaceId: string;
+  ownerId?: string;
+  description?: string;
+  coord?: any;
+  address?: string;
+  phone?: string;
+  cover?: string;
+  profilephoto?: string;
 };
 
 export type PlacePaginated = {
-    places: PlaceData[];
-    total: number;
+  places: PlaceData[];
+  total: number;
 };
 
 export class PlaceEntity {
-    createdById: string;
-    name: string;
-    active?: boolean;
-    createdAt?: Date;
-    updatedAt?: Date;
-    constructor(data: PlaceData) {
-        this.createdById = data.createdById;
-        this.name = data.name;
-        this.active = false;
-        this.createdAt = new Date();
-        this.updatedAt = new Date();
-    }
+  createdById: string;
+  name: string;
+  active?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  categoryPlaceId: string;
+  ownerId?: string;
+  description?: string;
+  coord?: any;
+  address?: string;
+  phone?: string;
+  cover?: string;
+  profilephoto?: string;
+  constructor(data: PlaceData) {
+    this.createdById = data.createdById;
+    this.name = data.name;
+    this.active = false;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+    this.categoryPlaceId = data.categoryPlaceId;
+    this.ownerId = data.ownerId;
+    this.description = data.description;
+    this.coord = data.coord;
+    this.address = data.address;
+    this.phone = data.phone;
+    this.cover = data.cover;
+    this.profilephoto = data.profilephoto;
+  }
 }
