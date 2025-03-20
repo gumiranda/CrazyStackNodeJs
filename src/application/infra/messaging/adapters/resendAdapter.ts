de5987329bf533f056064d83afcb12006b83b9ba
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-20 19:03:10 -0300

    fix: Correct email sender format in ResendAdapter

--- src/application/infra/messaging/adapters/resendAdapter.ts
+++ src/application/infra/messaging/adapters/resendAdapter.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export class ResendAdapter implements EmailAdapter {
   async sendEmail(to: string, subject: string, html: string): Promise<any> {
     const { data, error } = await resend.emails.send({
-      from: "Belezix <noreply@belezix.com",
+      from: "Belezix <noreply@belezix.com>",
       to: [to],
       subject,
       html,
