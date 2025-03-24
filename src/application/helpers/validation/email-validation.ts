f46adb50351cc6c9f648f378c903e73ca9b0aab1
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-24 15:40:03 -0300

    feat: Add environment-based UUID validation for MongoDB

--- src/application/helpers/validation/email-validation.ts
+++ src/application/helpers/validation/email-validation.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from "@/application/errors";
 import { Validation } from "@/application/helpers/contracts";
+import { env } from "@/application/infra/config/env";
 
 export class EmailValidation implements Validation {
   constructor(private readonly fieldName: string) {
@@ -14,6 +15,9 @@ export class EmailValidation implements Validation {
   }
 }
 export const isValidUUID = (uuid: string) => {
+  if (env.database === "mongodb") {
+    return uuid?.length > 20;
+  }
   const uuidRegex =
     /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
   return uuidRegex.test(uuid);
